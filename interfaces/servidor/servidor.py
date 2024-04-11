1cd8a2f34c9a958e94c031edb71ab4015c52b901
Silvio <silviozv.oli@gmail.com>
2024-04-10 21:17:53 -0300

    ✨feat: Mudanças no servidor.
    Colocando os dados em objetos de classes ao invés de variáveis globais. Adicionando a função para ler dados via udp a todo momento.

--- interfaces/servidor/servidor.py
+++ interfaces/servidor/servidor.py
@@ -1,29 +1,47 @@
-import socket 
+import threading
+from classe import Armazenamento, Conexao_servidor
 
-# ógica embaralhada
+# lógica embaralhada
 # recebe as conexões; armazena os sockets e printa; o comando de resposta é um rascunho
 
-dispositivos = []
+# É para colocar como global?
 
-porta_tcp = 5050
-ip_local = socket.gethostbyname( socket.gethostname())
-print(ip_local)
-endereco = (ip_local, porta_tcp)
+armazenamento = Armazenamento()
+conexao_servidor = Conexao_servidor()
+print(conexao_servidor.ip_servidor)
 
-servidor_tcp = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-servidor_tcp.bind( endereco)
+# Função temporária para enviar comandos ao dispositivo !!!! MUDEI PARA DICIONARIO
+def enviar_comando(endereco):
 
-servidor_tcp.listen()
+    while True:
 
+        comando = input("\nComando: ")
+        armazenamento.conexoes[endereco].send(comando.encode('utf-8'))
+        print(armazenamento.conexoes[endereco].recv(2048).decode('utf-8'))
+
+# Aceitar dispositivos que iniciam conexões (parece que funciona) 
 def receber_conexao():
     
     while True:
 
-        remetente, endereco_remetente = servidor_tcp.accept()
-        dispositivos.append(remetente)
+        remetente, endereco_remetente = conexao_servidor.servidor_tcp.accept()
+        armazenamento.conexoes[endereco_remetente[0]] = remetente
         print("Nova conexao:", endereco_remetente)
-        remetente.send("2".encode('utf-8'))
-        print(remetente.recv(2048).decode('utf-8'))
+        enviar_comando(endereco_remetente[0])
 
-receber_conexao()
+# Receber os dados enviados por udp (parece que funciona)
+def receber_dados():
 
+    while True:
+
+        dados, endereco = conexao_servidor.servidor_udp.recvfrom(2048)
+        print(dados.decode('utf-8'))
+        print(endereco[0])
+        armazenamento.dados_dispositivos[endereco[0]] = dados.decode('utf-8')
+
+def iniciar():
+
+    threading.Thread(target=receber_dados).start()
+    receber_conexao()
+
+iniciar()
