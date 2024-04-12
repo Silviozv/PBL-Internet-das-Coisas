9f373dc1d8cd2cf0328fbcfa5281818de89fd497
Silvio <silviozv.oli@gmail.com>
2024-04-12 18:46:16 -0300

    ✨feat (Server): Adicionando requisições de ligar e desligar o dispsitivo..

--- interfaces/servidor/servidor.py
+++ interfaces/servidor/servidor.py
@@ -10,6 +10,24 @@ storage = Storage()
 connection_server = Connection_server()
 print(connection_server.server_ip)
 
+# Funcção de teste de requisições
+def enviar_comando():
+
+    while True:
+        ip_device = input("IP dispositivo: ")
+        comando = int(input("Comando: "))
+
+        if comando == 1:
+            print(get_available_commands(ip_device))
+        elif comando == 2:
+            print(get_general_description(ip_device))
+        elif comando == 3:
+            print(turn_on_device(ip_device))
+        elif comando == 4:
+            print(turn_off_device(ip_device))
+        elif comando == 5:
+            print(get_data_udp(ip_device))
+
 # Aceitar dispositivos que iniciam conexões (parece que funciona) (é preciso retirar o print depois e a função coletar)
 def receive_connection_tcp():
     
@@ -19,7 +37,6 @@ def receive_connection_tcp():
         with connection_server.lock:
             storage.connections[address_sender[0]] = connection_sender
         print("Nova conexao:", address_sender)
-        print(get_available_commands(address_sender[0]))
 
 # Receber os dados enviados por udp (parece que funciona) (retirar os prints depois)
 def receive_data_udp():
@@ -50,16 +67,28 @@ def receive_data_udp():
 # Comando de retorno dos comandos disponíveis
 def get_available_commands(device_ip: str) -> str:
 
-    storage.connections[device_ip].send("2".encode('utf-8')) 
+    storage.connections[device_ip].send("1".encode('utf-8')) 
     available_commands = storage.connections[device_ip].recv(2048).decode('utf-8')
     return available_commands
 
 # Comando de retorno da descrição geral do dipositivo
 def get_general_description(device_ip: str) -> str:
 
-    storage.connections[device_ip].send("1".encode('utf-8')) 
+    storage.connections[device_ip].send("2".encode('utf-8')) 
     general_description = storage.connections[device_ip].recv(2048).decode('utf-8')
     return general_description
+
+def turn_on_device( device_ip: str) -> str:
+
+    storage.connections[device_ip].send("3".encode('utf-8')) 
+    response = storage.connections[device_ip].recv(2048).decode('utf-8')
+    return response
+
+def turn_off_device( device_ip: str) -> str:
+
+    storage.connections[device_ip].send("4".encode('utf-8')) 
+    response = storage.connections[device_ip].recv(2048).decode('utf-8')
+    return response
 
 # Comando de retorno do dado UDP
 # Coletar o dado retornado via UDP (parece que funciona mas tem que funcionar com a api)
@@ -74,11 +103,12 @@ def get_data_udp( device_ip: str) -> str:
     
     elif ( status == 'desligado'):
 
-        return "Dispositivo desligado"
-
+        return "Dispositivo desligado, não é possível coletar dados de leitura"
+    
 def iniciar():
 
+    threading.Thread(target=receive_connection_tcp).start()
     threading.Thread(target=receive_data_udp).start()
-    receive_connection_tcp()
+    enviar_comando()
 
 iniciar()
