63e043a725965f2f25be63dfe9b5682697907824
Silvio <silviozv.oli@gmail.com>
2024-04-11 22:23:10 -0300

    ✨feat (Class): Adicionando atributos e métodos.
    Atributo de descrição do dispositivo; Método para retornar informações a serem enviadas via UDP. Método para reiniciar o socket da conexão TCP.

--- interfaces/dispositivo/classe.py
+++ interfaces/dispositivo/classe.py
@@ -6,8 +6,19 @@ class Dispositivo:
     def __init__(self):
 
         self.ip_local = socket.gethostbyname( socket.gethostname())
+        self.descricao = 'Sensor de temperatura'
         self.status = 'desligado'
         self.temperatura = 0
+
+    # Dados a serem enviados via UDP
+    def get_informacoes(self) -> dict:
+
+        informacoes = {"Temperatura": self.temperatura}
+        return informacoes
+    
+    def get_status(self):
+
+        return self.status
 
 class Conexao:
 
@@ -22,3 +33,7 @@ class Conexao:
         self.ip_servidor = ""
         self.servidor_conectado = False
         self.lock = threading.Lock()
+
+    def reiniciar_conexao_tcp(self):
+
+        self.dispositivo_tcp = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
