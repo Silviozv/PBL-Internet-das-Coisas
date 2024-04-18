bd5c87ff948bed1f59e1cf01cf5ea7d6006096a9
Silvio <silviozv.oli@gmail.com>
2024-04-18 16:14:08 -0300

    ✨feat (Device): Colocando a validação de comunicação.

--- interfaces/broker/API.py
+++ interfaces/broker/API.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import servidor 
 import threading
 
@@ -8,38 +8,54 @@ app = Flask(__name__)
 @app.route('/devices/',methods=['GET'])
 def get_devices_id():
 
-    servidor.validate_communication()
-    return jsonify(servidor.get_devices_id())
+    devices_id = servidor.get_devices_id()
+    for i in range(len(devices_id)):
+        servidor.validate_communication(devices_id[i])
+
+    return jsonify(servidor.get_devices_id()), 200
 
 # Enviar comando do tipo de retorno de dados
 @app.route('/devices/<string:device_id>/commands/description',methods=['GET'])
 def get_device_commands_description( device_id):
-    servidor.validate_communication()
-    return jsonify(servidor.get_device_commands_description(device_id))
+
+    connected = servidor.validate_communication(device_id)
+    if ( connected == True):
+        return jsonify(servidor.get_device_commands_description(device_id)), 200
+    elif ( connected == False):
+        return jsonify ({'Tipo de resposta': 'Mensagem de resposta', 'Resposta': 'Dispositivo não encontrado'}), 404
 
 # Enviar comando do tipo de retorno de dados
 @app.route('/devices/<string:device_id>/commands/<string:command>',methods=['GET'])
 def get_device_data( device_id, command):
 
-    servidor.validate_communication()
-    request = {'Comando': command}
-    return jsonify(servidor.send_command(device_id, request))
+    connected = servidor.validate_communication(device_id)
+    if ( connected == True):
+        request = {'Comando': command}
+        return jsonify(servidor.send_command(device_id, request)), 200
+    elif ( connected == False):
+        return jsonify ({'Tipo de resposta': 'Mensagem de resposta', 'Resposta': 'Dispositivo não encontrado'}), 404
 
 # Enviar comando do tipo mudança de estado
 @app.route('/devices/<string:device_id>/commands/<string:command>',methods=['POST'])
 def set_device_state( device_id, command):
 
-    servidor.validate_communication()
-    request = {'Comando': command}
-    return jsonify(servidor.send_command(device_id, request))
+    connected = servidor.validate_communication(device_id)
+    if ( connected == True):
+        request = {'Comando': command}
+        return jsonify(servidor.send_command(device_id, request))
+    elif ( connected == False):
+        return jsonify ({'Tipo de resposta': 'Mensagem de resposta', 'Resposta': 'Dispositivo não encontrado'}), 404
 
 # Enviar comando do tipo mudar dado específico
 @app.route('/devices/<string:device_id>/commands/<string:command>',methods=['PATCH'])
 def set_device_data( device_id, command):
 
-    servidor.validate_communication()
-    request = {'Comando': command}
-    return jsonify(servidor.send_command(device_id, request))
+    connected = servidor.validate_communication(device_id)
+    if ( connected == True):
+        request = {'Comando': command}
+        return jsonify(servidor.send_command(device_id, request))
+    elif ( connected == False):
+        return jsonify ({'Tipo de resposta': 'Mensagem de resposta', 'Resposta': 'Dispositivo não encontrado'}), 404
 
 threading.Thread( target=servidor.receive_connection_tcp).start()
 threading.Thread( target=servidor.receive_data_udp).start()
