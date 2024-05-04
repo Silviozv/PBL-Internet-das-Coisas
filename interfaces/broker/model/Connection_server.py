61f745c6ed1fd92a60b184241df6e8d6c4f6741f
Silvio <silviozv.oli@gmail.com>
2024-05-04 13:22:49 -0300

    📚docs (Broker): Comentando arquivos do model.

--- interfaces/broker/model/Connection_server.py
+++ interfaces/broker/model/Connection_server.py
@@ -1,10 +1,21 @@
+"""
+Módulo contendo a classe que representa os dados de conexão do servidor.
+"""
+
 import socket
 import threading
 
 
 class Connection_server:
+    """ Classe que representa os dados de conexão do servidor. """
 
     def __init__(self):
+        """
+        Inicialização dos atributos base de conexão do servidor. Incluindo: 
+        o objeto de comunicação TCP para aceitar conexões; o objeto de comunicação 
+        UDP para recebimento de dados; as portas indicadas para comunicação TCP e UDP; 
+        e o objeto 'lock' utilizado evitar condições de corrida.
+        """
 
         self.server_ip = socket.gethostbyname( socket.gethostname())
 
--- interfaces/broker/model/Storage.py
+++ interfaces/broker/model/Storage.py
@@ -1,6 +1,27 @@
+"""
+Módulo contendo a classe que representa o armazenamento do servidor Broker.
+"""
+
+
 class Storage:
+    """ Classe que representa o armazenamento do servidor Broker. """
 
     def __init__(self):
+        """
+        Inicialização das estruturas de armazenamento do servidor Broker. 
+        Descrição das estruturas:
+
+        - connections: Armazena os objetos de comunicação TCP com os dispositivos, 
+          usando o IP como chave;
+        - connections_id: Armazena o IP dos dispositivos conectados, usando o ID 
+          como chave;
+        - devices_commands_description: Armazena as informações das requisições de 
+          cada dispositivo conectado, usando o IP como chave;
+        - data_udp_devices: Armazena os dados recebidos via comunicação UDP, usando 
+          o IP como chave;
+        - flags_devices: Armazena as 'flags' de controle dos objetos de comunicação TCP, 
+          usando o IP como chave.
+        """
 
         self.connections = {}
         self.connections_id = {}
