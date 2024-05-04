ced28529601cc07d897f3991d4f67d782b0a9e70
Silvio <silviozv.oli@gmail.com>
2024-05-04 13:10:37 -0300

    📚docs (Device): Comentando arquivos de inicialização;

--- interfaces/dispositivo/exec/radio/__main__.py
+++ interfaces/dispositivo/exec/radio/__main__.py
@@ -1,3 +1,7 @@
+"""
+Módulo de inicialização do Radio.
+"""
+
 import threading  
 import __init__
 
@@ -5,7 +9,12 @@ from model.Radio import Radio
 from model.Connection_device import Connection_device
 from impl.radio.RadioImpl import server_request_tcp, menu
 
+
 if __name__=="__main__":
+    """
+    Inicialização das funções principais do dispositivo, e declaração dos objetos de 
+    representação do Radio e da sua conexão com o servidor.
+    """
 
     radio = Radio()
     connection = Connection_device()
@@ -13,4 +22,3 @@ if __name__=="__main__":
     # A lógica de receber comandos e poder executar opções do menu parecem funcionar
     threading.Thread(target=server_request_tcp, args=[ radio, connection]).start()
     menu( radio, connection)
-
--- interfaces/dispositivo/exec/sensor/__main__.py
+++ interfaces/dispositivo/exec/sensor/__main__.py
@@ -1,3 +1,7 @@
+"""
+Módulo de inicialização do Sensor de temperatura.
+"""
+
 import threading  
 import __init__
 
@@ -5,7 +9,12 @@ from model.Sensor import Sensor
 from model.Connection_device import Connection_device
 from impl.sensor.SensorImpl import server_request_tcp, send_data_udp, menu
 
+
 if __name__=="__main__":
+    """
+    Inicialização das funções principais do dispositivo, e declaração dos objetos de 
+    representação do Sensor de temperatura e da sua conexão com o servidor.
+    """
 
     sensor = Sensor()
     connection = Connection_device()
@@ -14,4 +23,3 @@ if __name__=="__main__":
     threading.Thread(target=server_request_tcp, args=[ sensor, connection]).start()
     threading.Thread(target=send_data_udp, args=[ sensor, connection]).start()
     menu( sensor, connection)
-
